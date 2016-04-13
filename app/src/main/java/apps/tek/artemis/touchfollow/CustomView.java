4fdf810bab7b38c8f26ca1a32acd19850545c70a
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-04-13 17:25:40 -0400

    Added

---
+++ app/src/main/java/apps/tek/artemis/touchfollow/CustomView.java
@@ -0,0 +1,276 @@
+package apps.tek.artemis.touchfollow;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by viktor on 16-03-28.
+ */
+public class CustomView extends SurfaceView implements Runnable {
+
+
+    Thread th = null;
+    SurfaceHolder sh;
+    Canvas canvas = null;
+    Thread animationFrame;
+    Thread addEnemy;
+    boolean enemyAdd = false;
+    private float y, x;
+    private Player pl;
+    private boolean runEnemy;
+    private ArrayList<Spikes> spikes;
+    private int enemy = 1;
+    private boolean running, anim;
+    private int stage = 0;
+    private long speedMS = 1;
+    private long frames;
+    private Paint background;
+    Rect[] spikeRect;
+    private int score;
+    private boolean turningOff = true;
+
+    //
+
+    public CustomView(Context context) {
+        super(context);
+
+        sh = getHolder();
+        pl = new Player(BitmapFactory.decodeResource(getResources(), R.drawable.bobbyv1po1));
+        initialiseEnemy(enemy);
+
+        background = new Paint();
+        background.setColor(Color.WHITE);
+
+
+    }
+
+    private void initialiseEnemy(int enemyNum) {
+        if (spikes == null) {
+            spikes = new ArrayList<>(enemyNum);
+        }
+        if (spikes != null) {
+            spikes.add(new Spikes(BitmapFactory.decodeResource(getResources(), R.drawable.spikesv1)));
+        }
+        /**
+         for (int i = 0; i < enemyNum; i++) {
+         if(spikes[i] == null) {
+         spikes[i] = new Spikes(BitmapFactory.decodeResource(getResources(), R.drawable.spikesv1));
+         }
+         }
+         */
+
+    }
+
+
+    public void draw() {
+        if (sh.getSurface().isValid()) {
+            canvas = sh.lockCanvas();
+            canvas.drawPaint(background);
+            pl.draw(canvas);
+
+            for (int i = 0; i < spikes.size(); i++) {
+                if (spikes.get(i) != null) {
+                    spikes.get(i).update();
+                    spikes.get(i).draw(canvas);
+                }
+            }
+
+
+            sh.unlockCanvasAndPost(canvas);
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = event.getX();
+        y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                anim = true;
+                pl.update(x, y + 15);
+            //    System.out.println("" + x + " " + y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                pl.update(x, y + 15);
+           //     System.out.println("" + x + " " + y);
+                break;
+            case MotionEvent.ACTION_UP:
+                anim = false;
+                pl.update(x, y + 15);
+           //     System.out.println("" + x + " " + y);
+                break;
+            default:
+                return false;
+
+        }
+        invalidate();
+        return true;
+
+    }
+
+
+    @Override
+    public void run() {
+        while (running) {
+            draw();
+            if (turningOff) {
+                checkCollision();
+            }
+            frames++;
+         //   System.out.println(frames);
+
+
+            try {
+                //  System.out.println("slept");
+                Thread.sleep(speedMS);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void checkCollision() {
+        Rect player = pl.returnBounds();
+
+        //initiaise the rectangles
+
+
+        spikeRect = new Rect[spikes.size()];
+
+        for (int s = 0; s < spikes.size(); s++) {
+            if (spikes.get(s) != null) {
+                spikeRect[s] = spikes.get(s).returnBounds();
+            }
+        }
+
+        if (frames > 10) {
+            System.out.println(frames);
+            for (int i = 0; i < spikeRect.length; i++) {
+
+                if (spikeRect[i] != null && player != null) {
+                    System.out.println(i);
+                    if (Rect.intersects(player, spikeRect[i])) {
+                        resetGame();
+                        //enemy = 1;
+                        frames = 0;
+
+                    } else {
+                        addEnemyThread();
+
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+
+    private void addEnemyThread() {
+
+        /**
+         * Im fairly sure there is a better way of doing this but who gives a shit.
+         * I mean its my program and I can decide how to write it.
+         * Also... WHY THE FUCK ARN'T THERE TUTORIALS ON THIS SHIT
+         * I mean, really. I have looked all over on the internet looking for game programing,
+         * then finally i give up and create my own method.
+         *      -Viktor Korolyuk Age 15 (Thu,11:01pm,April,7,2016)
+         */
+
+        if (!enemyAdd) {
+            runEnemy = true;
+            addEnemy = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    while (runEnemy) {
+                        enemy++;
+                        initialiseEnemy(enemy);
+                        try {
+                            Thread.sleep((long) 6000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            });
+            addEnemy.start();
+            enemyAdd = true;
+        }
+
+    }
+
+    private void resetGame() {
+        pl = new Player(BitmapFactory.decodeResource(getResources(), R.drawable.bobbyv1po1));
+        spikes = null;
+        enemy = 1;
+        initialiseEnemy(enemy);
+        runEnemy = false;
+        enemyAdd = false;
+        try {
+            addEnemy.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
+        turningOff = true;
+    }
+
+
+    public void resume() {
+        running = true;
+        th = new Thread(this);
+        th.start();
+        animationFrame = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (running) {
+                    if (anim) {
+                        if (stage == 0) {
+                            pl.changeState(BitmapFactory.decodeResource(getResources(), R.drawable.bobbyv1po1));
+                            stage = 1;
+                        } else if (stage == 1) {
+                            pl.changeState(BitmapFactory.decodeResource(getResources(), R.drawable.bobbyv1po2));
+                            draw();
+                            stage = 0;
+                        }
+                        try {
+                            Thread.sleep(100);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else if (!anim) {
+                        pl.changeState(BitmapFactory.decodeResource(getResources(), R.drawable.bobbyv1po1));
+                    }
+                }
+            }
+        });
+        animationFrame.start();
+    }
+
+    public void pause() {
+        running = false;
+        try {
+            th.join();
+            animationFrame.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
+    }
+}
---
+++ app/src/main/java/apps/tek/artemis/touchfollow/Player.java
@@ -0,0 +1,60 @@
+package apps.tek.artemis.touchfollow;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+/**
+ * Created by viktor on 16-03-29.
+ */
+public class Player {
+    private Bitmap player;
+    private float x, y, playerWidth, playerHeight;
+    private boolean alive,ran = true;
+
+
+    public Player(Bitmap res) {
+        this.player = res;
+
+        alive = true;
+    }
+
+    public void update(float x, float y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public void draw(Canvas canvas) {
+        if (alive) {
+            player = Bitmap.createScaledBitmap(player, canvas.getWidth() / 4, (int) ((double) (canvas.getWidth() / 4) / 0.86), false);
+            getScale();
+            x = x - (playerWidth / 2);
+            canvas.drawBitmap(player, x, y, null);
+        }
+
+
+    }
+
+    private void getScale() {
+        if(ran){
+            playerWidth = player.getWidth();
+            playerHeight = player.getHeight();
+            ran = false;
+        }
+    }
+
+    public void changeState(Bitmap b) {
+
+        this.player = b;
+    }
+
+    public Rect returnBounds() {
+
+        return new Rect((int) x, (int) y, (int) x + ((int) (playerWidth / 2)), (int) y + ((int) playerHeight));
+    }
+
+
+}
